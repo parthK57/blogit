5249b9ec2b46cc31b5a25150bb2b3480ee866f16
parthK57 <parth.kolgiri32@gmail.com>
2023-04-02 19:12:16 +0530

    modified users controller

--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -2,6 +2,7 @@ import ErrorHandler from "../Services/ErrorHandler";
 import db from "../database/db";
 import { Encrypter } from "../Services/Bcrypt";
 import { signUpBody } from "../modals/users";
+import TimeStamp from "../Services/TimeStamp";
 
 export const loginHandler = (req: any, res: any, next: any) => {
   res.status(200).send("OK!");
@@ -13,8 +14,9 @@ export const signUpHandler = async (req: any, res: any, next: any) => {
   const fullname = body.fullname;
   const email = body.email;
   const password = body.password;
-  // console.log(fullname, username, email, password);
+  
   const hashedPassword = await Encrypter(password);
+  const timestamp = TimeStamp();
 
   try {
     const [userData] = (await db.execute(
@@ -25,8 +27,8 @@ export const signUpHandler = async (req: any, res: any, next: any) => {
       next(new ErrorHandler("User Already Exists!", 400));
     else {
       await db.execute(
-        "INSERT INTO users (fullname, username, email, password) VALUES (?,?,?,?);",
-        [fullname, username, email, hashedPassword]
+        "INSERT INTO users (full_name, user_name, email, password, date_created) VALUES (?,?,?,?,?);",
+        [fullname, username, email, hashedPassword, timestamp]
       );
       res.status(201).send("OK!");
     }
