fae88f3e41f8a82c3f470a3387cebedc8e12955e
parthK57 <parth.kolgiri32@gmail.com>
2023-04-03 13:27:04 +0530

    redux/slice - userslice

---
+++ frontend/src/slices/UserSlice.tsx
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+let userObject = {
+  userName: "",
+  fullName: "",
+  title: "",
+  age: "",
+  location: "",
+  education: "",
+  profilePicture: "",
+  facebook: "",
+  instagram: "",
+  twitter: "",
+  linkedIn: "",
+  github: "",
+  gitlab: "",
+};
+
+export const UserSlice = createSlice({
+  name: "userSlice",
+  initialState: {
+    value: userObject,
+  },
+  reducers: {
+    setUserData: (state, action) => {
+      state.value = action.payload;
+    },
+    clearUserData: (state) => {
+      state.value = userObject;
+    },
+  },
+});
+
+export const { setUserData, clearUserData } = UserSlice.actions;
