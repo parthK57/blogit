98b7f83d35205d7181ab67007cc0ab6bcfd22e3d
parthK57 <parth.kolgiri32@gmail.com>
2023-04-13 13:57:43 +0530

    page/home - component/sidebar - setting up modal for editting user data

--- frontend/src/pages/Home/Sidebar.tsx
+++ frontend/src/pages/Home/Sidebar.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { userObject } from "../../slices/UserSlice";
 
 // COMPONENTS
@@ -13,108 +13,133 @@ import {
 import { HiLocationMarker } from "react-icons/hi";
 import { HiBuildingOffice2 } from "react-icons/hi2";
 import { BsThreeDotsVertical } from "react-icons/bs";
+import { setUserEditModalIsActiveState } from "../../slices/ModalSlice";
 
 const Sidebar = () => {
-  const userData: userObject = useSelector((state: any) => state.user.value[0]);
   const [dropDown, setDropDown] = useState(false);
-
+  const dispatch = useDispatch();
+  const userData: userObject = useSelector((state: any) => state.user.value[0]);
+  const editUserModalState: boolean = useSelector(
+    (state: any) => state.modals.value.userEditModalIsActiveState
+  );
+  console.log(editUserModalState);
   return (
-    <div className="w hidden flex-col items-center bg-sky-50 md:flex md:h-[calc(100vh-80px)] md:w-[20%]">
-      <div className="flex w-[100%] flex-col items-center px-3">
-        <div className="relative mt-20 flex w-[90%] flex-col rounded-[15px] bg-sky-100 px-4 text-[12px] lg:text-base">
-          <div className="flex w-[100%] items-center justify-center">
-            <div
-              className={`relative bottom-6 h-[55px] w-[55px] rounded-full bg-[url(${userData?.profile_picture})] bg-contain bg-no-repeat p-2 pb-0`}
-            />
-          </div>
-          <div className="absolute top-0 right-0 p-3">
-            <BsThreeDotsVertical
-              onClick={() => setDropDown(!dropDown)}
-              className="pointer-events-auto cursor-pointer text-lg"
-            />
-          </div>
-          {dropDown && (
-            <>
-              <div className="absolute top-10 flex h-[45px] w-[90%] cursor-pointer items-center rounded-md bg-white px-4 shadow-md hover:text-blue-700">
-                Edit
+    <>
+      <div className="w hidden flex-col items-center bg-sky-50 md:flex md:h-[calc(100vh-80px)] md:w-[20%]">
+        <div className="flex w-[100%] flex-col items-center px-3">
+          <div className="relative mt-20 flex w-[90%] flex-col rounded-[15px] bg-sky-100 px-4 text-[12px] lg:text-base">
+            <div className="flex w-[100%] items-center justify-center">
+              <div
+                className={`relative bottom-6 h-[55px] w-[55px] rounded-full bg-[url(${userData?.profile_picture})] bg-contain bg-no-repeat p-2 pb-0`}
+              />
+            </div>
+            <div className="absolute top-0 right-0 p-3">
+              <BsThreeDotsVertical
+                onClick={() => setDropDown(!dropDown)}
+                className="pointer-events-auto cursor-pointer text-lg"
+              />
+            </div>
+            {dropDown && (
+              <>
+                <div
+                  onClick={() => {
+                    dispatch(setUserEditModalIsActiveState(true));
+                    setDropDown(!dropDown);
+                  }}
+                  className="absolute top-10 flex h-[45px] w-[90%] cursor-pointer items-center rounded-md bg-white px-4 shadow-md hover:text-blue-700"
+                >
+                  Edit
+                </div>
+              </>
+            )}
+            <div className="flex w-[100%] gap-4 px-2 pb-1 transition-all duration-200 ease-in-out hover:scale-105">
+              <span>Name:</span>
+              <span className="hover:text-blue-700">{userData?.full_name}</span>
+            </div>
+            <div className="flex w-[100%] gap-2 px-2 py-1 transition-all duration-200 ease-in-out hover:scale-105">
+              <span>Title:</span>
+              <span className="hover:text-blue-700">
+                {userData?.title !== null ? userData?.title : "No Data"}
+              </span>
+            </div>
+            <div className="flex w-[100%] gap-2 px-2 py-1 transition-all duration-200 ease-in-out hover:scale-105">
+              <span>Age:</span>
+              <span className="hover:text-blue-700">
+                {userData?.age !== null ? userData?.age : "No Data"}
+              </span>
+            </div>
+            <div className="flex w-[100%] items-center gap-2 px-2 py-1 transition-all duration-200 ease-in-out hover:scale-105">
+              <span>
+                <HiBuildingOffice2 className="text-amber-700" />
+              </span>
+              <span className="hover:text-blue-700">
+                {userData?.education !== null ? userData?.education : "No Data"}
+              </span>
+            </div>
+            <div className="flex w-[100%] items-center gap-2 px-2 py-1 transition-all duration-200 ease-in-out hover:scale-105">
+              <span>
+                <HiLocationMarker className="text-red-500" />
+              </span>
+              <span className="hover:text-blue-700">
+                {userData?.location !== null ? userData?.location : "No Data"}
+              </span>
+            </div>
+            {userData?.github && (
+              <div className="flex w-[100%] items-center gap-2 px-2 py-1 transition-all duration-200 ease-in-out hover:scale-105">
+                <span>
+                  <AiFillGithub className="text-lg text-gray-600" />
+                </span>
+                <span className="hover:text-blue-700">{userData?.github}</span>
               </div>
-            </>
-          )}
-          <div className="flex w-[100%] gap-4 px-2 pb-1 transition-all duration-200 ease-in-out hover:scale-105">
-            <span>Name:</span>
-            <span className="hover:text-blue-700">{userData?.full_name}</span>
+            )}
+            {userData?.gitlab && (
+              <div className="flex w-[100%] items-center gap-2 px-2 py-1 transition-all duration-200 ease-in-out hover:scale-105">
+                <span>
+                  <AiFillGitlab className="text-lg text-[#fca326]" />
+                </span>
+                <span className="hover:text-blue-700">{userData?.gitlab}</span>
+              </div>
+            )}
+            {userData?.facebook && (
+              <div className="flex w-[100%] items-center gap-2 px-2 py-1 transition-all duration-200 ease-in-out hover:scale-105">
+                <span>
+                  <AiFillFacebook className="text-lg text-[#4267B2]" />
+                </span>
+                <span className="hover:text-blue-700">
+                  {userData?.facebook}
+                </span>
+              </div>
+            )}
+            {userData?.instagram && (
+              <div className="flex w-[100%] items-center gap-2 px-2 py-1 transition-all duration-200 ease-in-out hover:scale-105">
+                <span>
+                  <AiFillInstagram className="text-lg text-[#da4391]" />
+                </span>
+                <span className="hover:text-blue-700">
+                  {userData?.instagram}
+                </span>
+              </div>
+            )}
+            {userData?.twitter && (
+              <div className="flex w-[100%] items-center gap-2 px-2 py-1 transition-all duration-200 ease-in-out hover:scale-105">
+                <span>
+                  <AiFillTwitterSquare className="text-lg text-[#1DA1F2]" />
+                </span>
+                <span className="hover:text-blue-700">{userData?.twitter}</span>
+              </div>
+            )}
           </div>
-          <div className="flex w-[100%] gap-2 px-2 py-1 transition-all duration-200 ease-in-out hover:scale-105">
-            <span>Title:</span>
-            <span className="hover:text-blue-700">
-              {userData?.title !== null ? userData?.title : "No Data"}
-            </span>
-          </div>
-          <div className="flex w-[100%] gap-2 px-2 py-1 transition-all duration-200 ease-in-out hover:scale-105">
-            <span>Age:</span>
-            <span className="hover:text-blue-700">
-              {userData?.age !== null ? userData?.age : "No Data"}
-            </span>
-          </div>
-          <div className="flex w-[100%] items-center gap-2 px-2 py-1 transition-all duration-200 ease-in-out hover:scale-105">
-            <span>
-              <HiBuildingOffice2 className="text-amber-700" />
-            </span>
-            <span className="hover:text-blue-700">
-              {userData?.education !== null ? userData?.education : "No Data"}
-            </span>
-          </div>
-          <div className="flex w-[100%] items-center gap-2 px-2 py-1 transition-all duration-200 ease-in-out hover:scale-105">
-            <span>
-              <HiLocationMarker className="text-red-500" />
-            </span>
-            <span className="hover:text-blue-700">
-              {userData?.location !== null ? userData?.location : "No Data"}
-            </span>
-          </div>
-          {userData?.github && (
-            <div className="flex w-[100%] items-center gap-2 px-2 py-1 transition-all duration-200 ease-in-out hover:scale-105">
-              <span>
-                <AiFillGithub className="text-lg text-gray-600" />
-              </span>
-              <span className="hover:text-blue-700">{userData?.github}</span>
-            </div>
-          )}
-          {userData?.gitlab && (
-            <div className="flex w-[100%] items-center gap-2 px-2 py-1 transition-all duration-200 ease-in-out hover:scale-105">
-              <span>
-                <AiFillGitlab className="text-lg text-[#fca326]" />
-              </span>
-              <span className="hover:text-blue-700">{userData?.gitlab}</span>
-            </div>
-          )}
-          {userData?.facebook && (
-            <div className="flex w-[100%] items-center gap-2 px-2 py-1 transition-all duration-200 ease-in-out hover:scale-105">
-              <span>
-                <AiFillFacebook className="text-lg text-[#4267B2]" />
-              </span>
-              <span className="hover:text-blue-700">{userData?.facebook}</span>
-            </div>
-          )}
-          {userData?.instagram && (
-            <div className="flex w-[100%] items-center gap-2 px-2 py-1 transition-all duration-200 ease-in-out hover:scale-105">
-              <span>
-                <AiFillInstagram className="text-lg text-[#da4391]" />
-              </span>
-              <span className="hover:text-blue-700">{userData?.instagram}</span>
-            </div>
-          )}
-          {userData?.twitter && (
-            <div className="flex w-[100%] items-center gap-2 px-2 py-1 transition-all duration-200 ease-in-out hover:scale-105">
-              <span>
-                <AiFillTwitterSquare className="text-lg text-[#1DA1F2]" />
-              </span>
-              <span className="hover:text-blue-700">{userData?.twitter}</span>
-            </div>
-          )}
         </div>
+        {editUserModalState && (
+          <div
+            onClick={() => dispatch(setUserEditModalIsActiveState(false))}
+            className="absolute top-0 left-0 flex h-screen w-screen items-center justify-center bg-white/80"
+          >
+            Hola Bitch!
+          </div>
+        )}
       </div>
-    </div>
+    </>
   );
 };
 
